a51a3633bd84cdff58801f8768d5387b81cfd9d4
alisson oliveira ferreira <alisson@coopervap.com.br>
2019-04-25 13:19:27 -0300

    Criado apps, departamento, empresas e funcionarios

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.empresas'
+    'apps.empresas',
+    'apps.funcionarios',
+    'apps.departamentos'
 ]
 
 MIDDLEWARE = [
